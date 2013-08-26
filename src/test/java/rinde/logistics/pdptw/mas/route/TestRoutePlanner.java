0b88485a16e041e025f248d73920b2eada8244e9
rinde <Rinde.vanLon@cs.kuleuven.be>
2013-08-26 15:28:30 +0200

    tests update

--- src/test/java/rinde/logistics/pdptw/mas/route/TestRoutePlanner.java
+++ src/test/java/rinde/logistics/pdptw/mas/route/TestRoutePlanner.java
@@ -3,14 +3,12 @@
  */
 package rinde.logistics.pdptw.mas.route;
 
-import static com.google.common.base.Preconditions.checkState;
 import static com.google.common.collect.Lists.newLinkedList;
 
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Queue;
 
-import rinde.logistics.pdptw.mas.route.AbstractRoutePlanner;
 import rinde.sim.pdptw.common.DefaultParcel;
 
 /**
@@ -35,10 +33,10 @@ public class TestRoutePlanner extends AbstractRoutePlanner {
 
   @Override
   protected void doUpdate(Collection<DefaultParcel> onMap, long time) {
-    checkState(pdpModel != null && vehicle != null);
     @SuppressWarnings({ "unchecked", "rawtypes" })
     final Collection<DefaultParcel> inCargo = Collections
-        .checkedCollection((Collection) pdpModel.getContents(vehicle), DefaultParcel.class);
+        .checkedCollection((Collection) pdpModel.get().getContents(vehicle
+            .get()), DefaultParcel.class);
     route.clear();
     route.addAll(onMap);
     route.addAll(inCargo);
