57f931f0923a998be54f0a22788a6804bd69e0f6
Rinde van Lon <rindevanlon@gmail.com>
2016-04-11 10:49:39 +0200

    changed max auction duration to 10 minutes

--- src/main/java/com/github/rinde/logistics/pdptw/mas/comm/AuctionCommModel.java
+++ src/main/java/com/github/rinde/logistics/pdptw/mas/comm/AuctionCommModel.java
@@ -53,9 +53,8 @@ import com.google.common.base.Optional;
 public class AuctionCommModel<T extends Bid<T>>
     extends AbstractCommModel<Bidder<T>>
     implements TickListener {
-  // FIXME
-  private static final long MAX_AUCTION_DURATION_MS = 60000L;
-  // 5 * 60 * 1000L;
+
+  private static final long MAX_AUCTION_DURATION_MS = 10 * 60 * 1000L;
 
   final Map<Parcel, ParcelAuctioneer> parcelAuctioneerMap;
   final AuctionStopCondition<T> stopCondition;
